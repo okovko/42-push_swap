ca6ccf51659d73566d764c5259272eee3c416479
Oleg Kovalenko <olkovale@e1z1r4p20.42.us.org>
2017-10-10 01:47:39 -0700

    fixed the bug, also ft_lstapp is broken

--- libft/srcs/ft_lstmerge.c
+++ libft/srcs/ft_lstmerge.c
@@ -25,11 +25,18 @@ t_lst	*ft_lstmerge(t_lst **into, t_lst **outof,
 		lgt = ft_lstlgt(*into, pop, cmp);
 		glt = ft_lstglt(*into, pop, cmp);
 		if (NULL != lgt)
+		{
 			ft_lstadd(&lgt, pop);
+		}
 		else if (NULL != glt)
-			ft_lstadd(&glt->nxt, pop);
+		{
+			glt = glt->nxt;
+			ft_lstadd(&glt, pop);
+		}
 		else
+		{
 			ft_lstadd(into, pop);
+		}
 	}
 	return (ft_lstmin(*into, cmp));
 }
--- libft/srcs/ft_lstpop.c
+++ libft/srcs/ft_lstpop.c
@@ -6,7 +6,7 @@
 /*   By: olkovale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/06 16:30:31 by olkovale          #+#    #+#             */
-/*   Updated: 2017/10/09 18:18:53 by olkovale         ###   ########.fr       */
+/*   Updated: 2017/10/10 01:10:53 by olkovale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_lst	*ft_lstpop(t_lst **ll)
 	t_lst	*aa;
 	t_lst	*bb;
 	t_lst	*cc;
-	
+
 	if (NULL == ll || NULL == *ll)
 		return (NULL);
 	if (*ll == (*ll)->nxt)
@@ -32,11 +32,6 @@ t_lst	*ft_lstpop(t_lst **ll)
 	aa->nxt = cc;
 	cc->prv = aa;
 	*ll = cc;
-	/*
-	(*ll)->nxt->prv = (*ll)->prv;
-	(*ll)->prv->nxt = (*ll)->nxt;
-	*ll = (*ll)->nxt;
-	*/
 	bb->prv = bb;
 	bb->nxt = bb;
 	return (bb);
