29b10fa7bbe6ebcd989afc5cecb9ffdf9129cdc4
Oleg Kovalenko <olkovale@e1z1r4p20.42.us.org>
2017-10-19 04:15:16 -0700

    at least some cases work

--- srcs/merge.c
+++ srcs/merge.c
@@ -6,7 +6,7 @@
 /*   By: olkovale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/08 15:45:17 by olkovale          #+#    #+#             */
-/*   Updated: 2017/10/15 03:33:36 by olkovale         ###   ########.fr       */
+/*   Updated: 2017/10/18 19:49:44 by olkovale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 #include "push_swap.h"
 
-int				merge_a_helper(t_lst **aa, t_lst **bb)
+int				merge_a_put(t_lst **aa, t_lst **bb)
 {
 	op_pa(aa, bb);
 	ft_putstr("pa\n");
@@ -25,7 +25,7 @@ int				merge_a_helper(t_lst **aa, t_lst **bb)
 	return (2);
 }
 
-int				merge_b_helper(t_lst **aa, t_lst **bb)
+int				merge_b_put(t_lst **aa, t_lst **bb)
 {
 	op_pb(aa, bb);
 	ft_putstr("pb\n");
@@ -34,7 +34,21 @@ int				merge_b_helper(t_lst **aa, t_lst **bb)
 	return (2);
 }
 
-int				merge_a(t_lst **aa, t_lst **bb, int sz)
+int				merge_a_rot(t_lst **aa, t_lst **bb)
+{
+	op_ra(aa, bb);
+	ft_putstr("ra\n");
+	return (1);
+}
+
+int				merge_b_rot(t_lst **aa, t_lst **bb)
+{
+	op_rb(aa, bb);
+	ft_putstr("rb\n");
+	return (1);
+}
+
+int				merge_a_inner(t_lst **aa, t_lst **bb, int sz)
 {
 	int		ops;
 	int		ii;
@@ -47,23 +61,23 @@ int				merge_a(t_lst **aa, t_lst **bb, int sz)
 	{
 		if (ft_lstcmp_lli(*bb, *aa) < 0)
 		{
-			ops += merge_a_helper(aa, bb);
+			ops += merge_a_put(aa, bb);
 			ii++;
 		}
 		else
 		{
-			op_ra(aa, bb);
-			ft_putstr("ra\n");
-			ops++;
+			ops += merge_a_rot(aa, bb);
 			jj++;
 		}
 	}
 	while (ii++ < sz)
-		ops += merge_a_helper(aa, bb);
+		ops += merge_a_put(aa, bb);
+	while (jj++ < sz)
+		ops += merge_a_rot(aa, bb);
 	return (ops);
 }
 
-int				merge_b(t_lst **aa, t_lst **bb, int sz)
+int				merge_b_inner(t_lst **aa, t_lst **bb, int sz)
 {
 	int		ops;
 	int		ii;
@@ -76,19 +90,63 @@ int				merge_b(t_lst **aa, t_lst **bb, int sz)
 	{
 		if (ft_lstcmp_lli(*aa, *bb) < 0)
 		{
-			ops += merge_b_helper(aa, bb);
+			ops += merge_b_put(aa, bb);
 			ii++;
 		}
 		else
 		{
-			op_rb(aa, bb);
-			ft_putstr("rb\n");
-			ops++;
+			ops += merge_b_rot(aa, bb);
 			jj++;
 		}
 	}
 	while (ii++ < sz)
-		ops += merge_b_helper(aa, bb);
+		ops += merge_b_put(aa, bb);
+	while (jj++ < sz)
+		ops += merge_b_rot(aa, bb);
+	return (ops);
+}
+
+int				merge_a_outer(t_lst **aa, t_lst **bb, int sz)
+{
+	int		ops;
+	int		ii;
+
+	ii = 0;
+	ops = 0;
+	while (ii < sz)
+	{
+		if (ft_lstcmp_lli(*bb, (*aa)->prv) > 0 && ft_lstcmp_lli(*bb, *aa) < 0)
+		{
+			ops += merge_a_put(aa, bb);
+			ii++;
+		}
+		else
+		{
+			ops += merge_a_rot(aa, bb);
+		}
+	}
+	return (ops);
+}
+
+int				merge_b_outer(t_lst **aa, t_lst **bb, int sz)
+{
+	int		ops;
+	int		ii;
+
+	ii = 0;
+	ops = 0;
+	while (ii < sz)
+	{
+		if (ft_lstcmp_lli(*aa, *bb) < 0)
+		{
+			ops += merge_b_put(aa, bb);
+			ii++;
+		}
+		else
+		{
+			ops += merge_b_rot(aa, bb);
+		}
+	}
 	return (ops);
 }
 
@@ -159,40 +217,75 @@ int				sort_pass(t_lst **aa, t_lst **bb)
 	return (ops);
 }
 
+int				merge_inner(t_lst **aa, t_lst **bb, int sz)
+{
+	int		inner_sz;
+	int		ops;
+	t_bool	lever;
+
+	ops = 0;
+	inner_sz = MIN(ft_lstsz(*aa) / sz, ft_lstsz(*bb) / sz);
+	lever = inner_sz == 1;
+	while (inner_sz-- > 0)
+	{
+		if (0 == lever)
+		{
+			ops += merge_b_inner(aa, bb, sz);
+			lever = 1;
+		}
+		else if (1 == lever)
+		{
+			ops += merge_a_inner(aa, bb, sz);
+			lever = 0;
+		}
+	}
+	return (ops);
+}
+
+int				merge_outer(t_lst **aa, t_lst **bb, int sz)
+{
+	int		a_outer_sz;
+	int		ops;
+
+	ops = 0;
+	a_outer_sz = ft_lstsz(*aa) % (sz * 2);
+	ops += merge_b_inner(aa, bb, a_outer_sz);
+	return (ops);
+}
+
 int				merge_pass(t_lst **aa, t_lst **bb)
 {
 	int		ops;
-	int		ii;
-	int		jj;
 	int		sz;
-	t_bool	merge_to_a;
 
-	merge_to_a = true;
 	ops = 0;
 	sz = 3;
 	while (sz <= ft_lstsz(*aa) && sz <= ft_lstsz(*bb))
 	{
-		ii = 0;
-		jj = 0;
-		while (ii + sz <= ft_lstsz(*aa) && jj + sz <= ft_lstsz(*bb))
-		{
-			if (merge_to_a)
-			{
-				ops += merge_a(aa, bb, sz);
-				merge_to_a = false;
-			}
-			else
-			{
-				merge_to_a = true;
-				ops += merge_b(aa, bb, sz);
-			}
-			ii += sz;
-			jj += sz;
-		}
-		ops += merge_b(aa, bb, sz + ft_lstsz(*aa) % sz);
+		ops += merge_inner(aa, bb, sz);
+		ops += merge_outer(aa, bb, sz);
 		sz *= 2;
 	}
-	ops += merge_a(aa, bb, ft_lstsz(*bb));
+	while (ft_lstsz(*bb) > 0)
+	{
+		ops += merge_a_outer(aa, bb, 1);
+	}
+	return (ops);
+}
+
+int				rot_pass(t_lst **aa, t_lst **bb)
+{
+	int			ops;
+	t_lst		*min;
+
+	ops = 0;
+	min = ft_lstmin(*aa, ft_lstcmp_lli);
+	while (min != *aa)
+	{
+		op_rra(aa, bb);
+		ft_putstr("rra\n");
+		ops++;
+	}
 	return (ops);
 }
 
@@ -204,6 +297,7 @@ int				msort(t_lst **aa, t_lst **bb)
 	ops += push_pass(aa, bb);
 	ops += sort_pass(aa, bb);
 	ops += merge_pass(aa, bb);
+	ops += rot_pass(aa, bb);
 	return (ops);
 }
 
