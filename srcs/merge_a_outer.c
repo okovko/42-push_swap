73a0bee358442fe6a105da35145f7c1fa8c973dc
Oleg Kovalenko <olkovale@e1z1r4p20.42.us.org>
2017-10-20 12:59:22 -0700

    debugged max min placements

--- srcs/merge_a_outer.c
+++ srcs/merge_a_outer.c
@@ -12,31 +12,62 @@
 
 #include "push_swap.h"
 
-int				merge_a_outer(t_lst **aa, t_lst **bb, int sz)
+static int		merge_a_min(t_lst **aa, t_lst **bb)
 {
 	int		ops;
-	int		ii;
 	t_lst	*min;
+
+	ops = 0;
+	min = ft_lstmin2(ft_lstmin(*aa, ft_lstcmp_lli),
+					ft_lstmin(*bb, ft_lstcmp_lli), ft_lstcmp_lli);
+	if (min == *bb)
+		ops += merge_a_put(aa, bb);
+	return (ops);
+}
+
+static int		merge_a_max(t_lst **aa, t_lst **bb)
+{
+	int		ops;
 	t_lst	*max;
+	t_lst	*min;
 
 	min = ft_lstmin2(ft_lstmin(*aa, ft_lstcmp_lli),
 					ft_lstmin(*bb, ft_lstcmp_lli), ft_lstcmp_lli);
 	max = ft_lstmax2(ft_lstmax(*aa, ft_lstcmp_lli),
 					ft_lstmax(*bb, ft_lstcmp_lli), ft_lstcmp_lli);
-	ii = 0;
 	ops = 0;
-	while (ii < sz)
+	if (max == *bb && min == *aa)
+		ops += merge_a_put(aa, bb);
+	return (ops);
+}
+
+static int		merge_a_mid(t_lst **aa, t_lst **bb)
+{
+	int		ops;
+
+	ops = 0;
+	if (ft_lstcmp_lli(*bb, (*aa)->prv) > 0 && ft_lstcmp_lli(*bb, *aa) < 0)
+		ops += merge_a_put(aa, bb);
+	return (ops);
+}
+
+int				merge_a_outer(t_lst **aa, t_lst **bb, int sz)
+{
+	int		ops;
+	int		ii;
+	int		jj;
+
+	(void)sz;
+	ii = 0;
+	jj = 0;
+	ops = 0;
+	ops += merge_a_min(aa, bb);
+	ops += merge_a_mid(aa, bb);
+	ops += merge_a_max(aa, bb);
+	if (ops == 0)
 	{
-		if (min == *bb || max == *bb ||
-			(ft_lstcmp_lli(*bb, (*aa)->prv) > 0 && ft_lstcmp_lli(*bb, *aa) < 0))
-		{
-			ops += merge_a_put(aa, bb);
-			ii++;
-		}
-		else
-		{
-			ops += merge_a_rot(aa, bb);
-		}
+		ops += merge_a_rot(aa, bb);
+		jj++;
 	}
 	return (ops);
 }
